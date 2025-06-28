e0237e307387cae79f0ce865109d36e7c74b04cd
Felix_laodan <felix.dong.laodan@gmail.com>
2025-06-28 21:40:34 +0800

    删除前端多余的后端Java代码，给pom.xml添加注释

--- frontend/java/com/strayanimal/platform/forum/mapper/ForumCommentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.strayanimal.platform.forum.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.strayanimal.platform.forum.entity.ForumComment;
-import org.apache.ibatis.annotations.Mapper;
- 
-@Mapper
-public interface ForumCommentMapper extends BaseMapper<ForumComment> {
-} 
--- frontend/java/com/strayanimal/platform/forum/mapper/ForumPostMapper.java
+++
@@ -1,9 +0,0 @@
-package com.strayanimal.platform.forum.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.strayanimal.platform.forum.entity.ForumPost;
-import org.apache.ibatis.annotations.Mapper;
- 
-@Mapper
-public interface ForumPostMapper extends BaseMapper<ForumPost> {
-} 
